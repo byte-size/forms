dddcb2222946d887375c323403ff3b352ead3426
Kemal Yalcinkaya <kemalcany@gmail.com>
2020-03-03 15:11:21 +0100

    refactor: v-click-outside moved inside this module (for focus trap)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// Dependencies
+import vClickOutside from "v-click-outside";
 import FormContainer from "./Form.Container.vue";
 
 export default {
   install(Vue, options) {
     Vue.component("form-container", FormContainer);
+    Vue.use(vClickOutside);
   }
 };
