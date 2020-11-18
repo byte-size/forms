4c932e5628d54fa83a23930c2e339fffe0454e74
Kemal Yalcinkaya <kemalcany@gmail.com>
2020-11-18 12:20:51 +0100

    feat: InputContainer added as a static component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import vClickOutside from 'v-click-outside'
 
 // Components
 import Form from './Form.vue'
-// import InputContainer from './Input.Container.vue'
+import InputContainer from './Input.Container.vue'
 
 export default {
   install(Vue, options) {
     Vue.use(vClickOutside)
     Vue.component('bs-form', Form)
-    // Vue.component('bs-input-container', InputContainer)
+    Vue.component('bs-input-container', InputContainer)
     if (options.debug) Vue.prototype.$bsFormsDebug = true
   }
 }
